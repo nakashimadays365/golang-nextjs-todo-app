6a0b11db00184e4f068766bd3db0361c47533909
中嶋晋作 <nakashimadays365@nakashimaDays365.local>
2023-03-10 11:40:01 +0900

    switchを削除した

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,17 +2,11 @@ package middleware
 
 import (
 	"net/http"
-	"os"
 )
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		env := os.Getenv("APP_ENV")
-		switch {
-		case env == "dev":
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		}
-
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,UPDATE,OPTIONS")
